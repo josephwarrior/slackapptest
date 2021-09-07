e06cfdbf0c45a2daf43a1769d668c9acd4d06397
josephwarrior <guerrero.jo@husky.neu.edu>
2021-09-06 20:51:29 -0700

    modify test so that it passes for the 6th time

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const mooncakeSays =require('./index')
 
 test('mooncake knows what to say', () => {
-    expect(mooncakeSays('woooahhhhhhhh!')).toBe('(o.o) woooahhhhhhhh!');
+    expect(mooncakeSays('woooahhhhhhhhh!')).toBe('(o.o) woooahhhhhhhhh!');
 });
 
 test('mooncake is not sure what to say', () => {
