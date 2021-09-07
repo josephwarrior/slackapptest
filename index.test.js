3648f0ee508c8290604c3b2df036e7fa5e4ec6fd
josephwarrior <guerrero.jo@husky.neu.edu>
2021-09-06 17:41:58 -0700

    Modify test so that it fails

--- index.test.js
+++ index.test.js
@@ -1,7 +1,7 @@
 const mooncakeSays =require('./index')
 
 test('mooncake knows what to say', () => {
-    expect(mooncakeSays('woooahhh!')).toBe('(o.o) woooahhh!');
+    expect(mooncakeSays('woooahhh!')).toBe('(0.o) woooahhh!');
 });
 
 test('mooncake is not sure what to say', () => {
